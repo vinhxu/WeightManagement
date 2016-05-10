1cf19f2ddb9043859333dbe92cade02a1505bef3
vinhxu <vx1141986@gmail.com>
2016-05-10 22:35:06 +0300

    add user listview

--- app/src/main/java/com/example/vinhxu/weightmanagement/MainActivity.java
+++ app/src/main/java/com/example/vinhxu/weightmanagement/MainActivity.java
@@ -4,16 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static Button addUserButton;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onClick_addUserButton();
+        //String[] userNameArray = {"vinhxu","lnk","Danh","Khanh"};
+        List<String> userNameList = new ArrayList<String>();
+
+        userNameList.add("vinhxu");
+        userNameList.add("anybody");
+        userNameList.add("Danh");
+
+        String[] userNameArray = userNameList.toArray(new String[userNameList.size()]);
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userNameArray);
+
+        ListView listView = (ListView) findViewById(R.id.listView_userName);
+        listView.setAdapter(adapter);
     }
 
     public void onClick_addUserButton() {
